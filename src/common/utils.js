708064cab44faf79ca28e965183dee525bd3e8ad
aisuandebowen <1836140285@qq.com>
2021-12-25 22:16:05 +0800

    工具类添加N树最大深度方法，完成节点拖拽限制功能

--- src/common/utils.js
+++ src/common/utils.js
@@ -1,14 +1,22 @@
 // 工具类
 
-// 清空数据内容，保存一级键名
-export function clearData(data) {
-    for(let key in data) {
+/**
+ * 清空数据内容，保存一级键名
+ * @param {Object} data 对象
+ */
+function clearData(data) {
+    for (let key in data) {
         data[key] = ''
     }
 }
 
-// 防抖函数
-export function debounce(fn, delay = 1000) {
+/**
+ * 防抖函数
+ * @param {*} fn 需防抖的函数
+ * @param {number} delay 延迟时间
+ * @returns 
+ */
+function debounce(fn, delay = 1000) {
     let timer = null
 
     return function (...args) {
@@ -19,4 +27,28 @@ export function debounce(fn, delay = 1000) {
             fn.apply(this, args)
         }, delay)
     }
+}
+
+/**
+ * 获取N树最大深度
+ * @param {Object} node 树形节点
+ * @returns 
+ */
+function getMaxDepth(node) {
+    if (!node) {
+        return 0
+    }
+    let maxDepth = 0
+    const childNodes = node.childNodes
+    for (const childNode of childNodes) {
+        const childNodeMaxDepth = getMaxDepth(childNode)
+        maxDepth = Math.max(maxDepth, childNodeMaxDepth)
+    }
+    return maxDepth + 1
+}
+
+export {
+    debounce,
+    clearData,
+    getMaxDepth
 }
--- src/views/categoryManage/CategoryManage.vue
+++ src/views/categoryManage/CategoryManage.vue
@@ -1,10 +1,11 @@
 <template>
     <div class="block">
         <p>使用 scoped slot</p>
-        <el-tree :data="data" show-checkbox node-key="id" default-expand-all :expand-on-click-node="false"
-            @node-drag-start="handleDragStart" @node-drag-enter="handleDragEnter" @node-drag-leave="handleDragLeave"
-            @node-drag-over="handleDragOver" @node-drag-end="handleDragEnd" @node-drop="handleDrop" draggable
-            :allow-drop="allowDrop" :allow-drag="allowDrag">
+        <el-button type="primary" @click="rootAppend">添加根结点</el-button>
+        <el-tree :data="data" default-expand-all @node-drag-start="handleDragStart" @node-drag-enter="handleDragEnter"
+            @node-drag-leave="handleDragLeave" @node-drag-over="handleDragOver" @node-drag-end="handleDragEnd"
+            @node-drop="handleDrop" draggable :allow-drop="allowDrop" :allow-drag="allowDrag"
+            :expand-on-click-node="false">
             <span class="custom-tree-node" slot-scope="{ node, data }">
                 <span>{{ node.label }}</span>
                 <span>
@@ -21,6 +22,7 @@
     </div>
 </template>
 <script>
+    import { getMaxDepth } from 'common/utils'
     let id = 1000;
     export default {
         name: 'CategoryManage',
@@ -28,17 +30,7 @@
             const data = [{
                 id: 1,
                 label: '一级 1',
-                children: [{
-                    id: 4,
-                    label: '二级 1-1',
-                    children: [{
-                        id: 9,
-                        label: '三级 1-1-1'
-                    }, {
-                        id: 10,
-                        label: '三级 1-1-2'
-                    }]
-                }]
+                children: []
             }, {
                 id: 2,
                 label: '一级 2',
@@ -61,7 +53,7 @@
                 }]
             }];
             return {
-                data: JSON.parse(JSON.stringify(data))
+                data: JSON.parse(JSON.stringify(data)),
             }
         },
         created() {
@@ -69,7 +61,29 @@
         computed: {
         },
         methods: {
+            // jiShu(node) {
+            //     if(!node) {
+            //         return 0
+            //     }
+            //     let maxDepth = 0
+            //     const childNodes = node.childNodes
+            //     for(const childNode of childNodes) {
+            //         const childNodeMaxDepth = this.$options.methods.jiShu(childNode)
+            //         maxDepth = Math.max(maxDepth,childNodeMaxDepth)
+            //     }
+            //     return maxDepth + 1
+            // },
+            rootAppend() {
+                console.log(this.data);
+                const newChild = {
+                    id: id++,
+                    label: 'testtest',
+                    children: []
+                }
+                this.data.push(newChild)
+            },
             append(data) {
+                console.log(data);
                 const newChild = { id: id++, label: 'testtest', children: [] };
                 if (!data.children) {
                     this.$set(data, 'children', []);
@@ -78,35 +92,68 @@
             },
 
             remove(node, data) {
+                console.log(data);
+                console.log(node);
                 const parent = node.parent;
                 const children = parent.data.children || parent.data;
                 const index = children.findIndex(d => d.id === data.id);
                 children.splice(index, 1);
             },
             handleDragStart(node, ev) {
-                console.log('drag start', node);
+
+                // console.log('drag start', node);
             },
             handleDragEnter(draggingNode, dropNode, ev) {
-                console.log('tree drag enter: ', dropNode.label);
+                // if(dropNode.level >= 3) {
+                //     console.log('求怕累');
+                // }
+
+                // console.log(dropNode);
+                // console.log(ev);
+                // console.log('tree drag enter: ', dropNode.label);
             },
             handleDragLeave(draggingNode, dropNode, ev) {
-                console.log('tree drag leave: ', dropNode.label);
+                // console.log('tree drag leave: ', dropNode.label);
             },
             handleDragOver(draggingNode, dropNode, ev) {
-                console.log('tree drag over: ', dropNode.label);
+                // console.log('tree drag over: ', dropNode.label);
             },
             handleDragEnd(draggingNode, dropNode, dropType, ev) {
-                console.log('tree drag end: ', dropNode && dropNode.label, dropType);
+                console.log(ev);
+
+                // this.canDraggable = true
+                // console.log('已修改为可拖拽');
+                // console.log('tree drag end: ', dropNode && dropNode.label, dropType);
             },
             handleDrop(draggingNode, dropNode, dropType, ev) {
-                console.log('tree drop: ', dropNode.label, dropType);
+                // console.log('tree drop: ', dropNode.label, dropType);
+                // this.canDraggable = true
+                // console.log('已修改为可拖拽');
             },
             allowDrop(draggingNode, dropNode, type) {
-                if (dropNode.data.label === '二级 3-1') {
-                    return type !== 'inner';
-                } else {
-                    return true;
+                console.log('-----------------------------');
+                console.log('被拖拽的节点：', draggingNode.data.label);
+                console.log(draggingNode);
+                console.log('准备进入的节点:', dropNode.data.label);
+                console.log(dropNode);
+                const draggingNodeDepth = getMaxDepth(draggingNode)
+                const dropNodeDepth = getMaxDepth(dropNode)
+                console.log('被拖拽的节点 最大深度：', draggingNodeDepth);
+                console.log('准备进入的节点 最大深度：', dropNodeDepth);
+                const maxDepth = Math.max(dropNodeDepth, draggingNodeDepth)
+                if ((maxDepth + dropNode.level) > 3) {
+                    console.log('已修改为不可拖拽');
+                    console.log('-----------------------------');
+                    return false
                 }
+
+                return true
+
+                // if (dropNode.data.label === '二级 3-1') {
+                //     return type !== 'inner';
+                // } else {
+                //     return true;
+                // }
             },
             allowDrag(draggingNode) {
                 return draggingNode.data.label.indexOf('三级 3-2-2') === -1;
