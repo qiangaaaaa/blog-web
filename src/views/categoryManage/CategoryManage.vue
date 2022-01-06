7da84fbf31fc115b27a02e06715a6d42cbd4c08f
aisuandebowen <1836140285@qq.com>
2022-01-06 14:24:53 +0800

    标签管理页面布局

--- src/views/categoryManage/CategoryManage.vue
+++ src/views/categoryManage/CategoryManage.vue
@@ -1,8 +1,7 @@
 <template>
     <div class="block">
-        <p>使用 scoped slot</p>
-        <el-button type="primary" @click="rootAppend">添加根结点</el-button>
-        <el-button type="primary" @click="allExpand">全部展开</el-button>
+        <el-button type="primary" plain icon="el-icon-folder-add" @click="rootAppend">添加根结点</el-button>
+        <el-button type="primary" plain icon="el-icon-s-operation" @click="allExpand">全部展开</el-button>
         <el-tree :data="data" default-expand-all @node-drag-start="handleDragStart" @node-drag-enter="handleDragEnter"
             @node-drag-leave="handleDragLeave" @node-drag-over="handleDragOver" @node-drag-end="handleDragEnd"
             @node-drop="handleDrop" draggable :allow-drop="allowDrop" :allow-drag="allowDrag"
@@ -10,26 +9,12 @@
             <div class="custom-tree-node" slot-scope="{ node, data }">
                 <span>{{ node.label }}</span>
                 <span>
-                    <el-button type="text" size="mini" @click="() => append(node,data)">
-                        Append
-                    </el-button>
-                    <el-button type="text" size="mini" @click="() => remove(node, data)">
-                        Delete
-                    </el-button>
+                    <i class="el-icon-circle-plus-outline categoryButton" @click="() => append(node,data)">添加</i>
+                    <i class="el-icon-remove-outline categoryButton" @click="() => remove(node, data)">删除</i>
+                    <i class="el-icon-zoom-in categoryButton">编辑</i>
                 </span>
             </div>
         </el-tree>
-
-        <!-- <template>
-            <el-table :data="tableData" style="width: 100%">
-                <el-table-column prop="date" label="日期" width="180">
-                </el-table-column>
-                <el-table-column prop="name" label="姓名" width="180">
-                </el-table-column>
-                <el-table-column prop="address" label="地址">
-                </el-table-column>
-            </el-table>
-        </template> -->
     </div>
 </template>
 <script>
@@ -157,6 +142,44 @@
 <style scoped>
     /* 修改树形控件基础样式 */
     .block>>>.el-tree-node__content {
+        position: relative;
         height: 60px !important;
+    }
+
+    .block>>>.el-tree-node__content::after {
+        content: '';
+        position: absolute;
+        background-color: #EBEEF5;
+        z-index: 1;
+        left: 0;
+        bottom: 0;
+        width: 100%;
+        height: 1px;
+    }
+
+    /* 树形表格 */
+    .categoryTree {
+        margin-top: 20px;
+        padding: 10px;
+        border: 1px solid rgb(0 0 0 / 6%);
+        box-shadow: 0 1px 3px 0 rgb(0 0 0 / 12%), 0 0 3px 0 rgb(0 0 0 / 4%);
+    }
+
+    .custom-tree-node>span:nth-child(2) {
+        padding-left: 200px;
+    }
+
+    .categoryButton {
+        padding: 0 20px;
+        color: #70b3f5;
+    }
+
+    .custom-tree-node {
+        flex: 1;
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        font-size: 14px;
+        padding-right: 150px;
     }
 </style>
