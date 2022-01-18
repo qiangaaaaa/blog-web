67caf5d8e4218d72524f3242d57a464cf6d26cb6
aisuandebowen <1836140285@qq.com>
2022-01-18 16:50:57 +0800

    接口-获取所有文章

--- src/network/article.js
+++ src/network/article.js
@@ -1,1 +1,8 @@
 import {request} from './request'
+
+// 获取所有文章（分页）
+export function getAllArticle(page = 1, limit = 20, sidx = 'title', order = 'desc', key = '') {
+    return request({
+        url: `article/list?page=${page}&limit=${limit}&sidx=${sidx}&order=${order}&${key}`
+    })
+}
